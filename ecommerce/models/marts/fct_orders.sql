33d02437ebd3fb9673f8f3f878ceb1db34508ccb
Sherwin-14 <varghesesheriwn@gmail.com>
2026-04-20 21:07:57 +0530

    Made all models

---
+++ ecommerce/models/marts/fct_orders.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        merge_update_columns=['status', 'revneue', 'number_of_lines', 'total_units', 'customer_country']
+    )
+}}
+
+with orders_enriched as (
+    select * from {{ ref('int_orders_enriched') }}
+    {% if is_incremental() %}
+    where order_date > (select MAX(order_date) from {{ this}})
+    {% endif %}
+
+)
+
+
+, final as (
+
+    select 
+         order_id,
+         customer_id, 
+         order_date,
+         status,
+         order_total_amount as revenue,
+         line_count as number_of_lines,
+         customer_country,
+        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as order_key
+    from orders_enriched
+)
+
+select * from final
